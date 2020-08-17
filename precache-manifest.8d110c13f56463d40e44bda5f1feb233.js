f44eeb9ca9f078172a9eb3f9036af5ae3db9b414
subhadeep paul <9007664088r@gmail.com>
2020-08-18 03:12:42 +0530

    Updates

---
+++ precache-manifest.8d110c13f56463d40e44bda5f1feb233.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c145526beebac53e6c30ff719999622",
+    "url": "/SubhadeepPaul/index.html"
+  },
+  {
+    "revision": "efe111543c5b63889475",
+    "url": "/SubhadeepPaul/static/css/main.60546d5c.chunk.css"
+  },
+  {
+    "revision": "e5902ee7ac1bcc41b667",
+    "url": "/SubhadeepPaul/static/js/2.9d983241.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/SubhadeepPaul/static/js/2.9d983241.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efe111543c5b63889475",
+    "url": "/SubhadeepPaul/static/js/main.82eb7739.chunk.js"
+  },
+  {
+    "revision": "80feb21b7f3fca1fc2ab",
+    "url": "/SubhadeepPaul/static/js/runtime-main.43ce763c.js"
+  },
+  {
+    "revision": "3dd218e1bfae880f5d1bcbc8ad93daa9",
+    "url": "/SubhadeepPaul/static/media/MyAvatar.3dd218e1.jpg"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/SubhadeepPaul/static/media/coffee-apple.2dd213aa.jpg"
+  }
+]);
